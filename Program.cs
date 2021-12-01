c58c3c66c06ace2da8db4aff3a31531d2b717ca9
Volkmar Rigo <VolkmarR@users.noreply.github.com>
2021-12-01 08:11:45 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AdventOfCode;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+
+            Console.WriteLine("Which day do you want to initialize? Press enter to cancel.");
+            var dayString = Console.ReadLine();
+            if (string.IsNullOrEmpty(dayString))
+                return;
+
+            if (int.TryParse(dayString, out var day))
+            {
+                DayInitialization.Execute(day);
+                return;
+            }
+        }
+
+    }
+}
