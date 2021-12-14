f112b5d5ceb3609b7e1cc9398d0daa0a86758f9c
Volkmar Rigo <volkmar.rigo@infominds.eu>
2021-12-14 21:53:21 +0100

    New approach for Part 2

--- Day14/Day14Solver.cs
+++ Day14/Day14Solver.cs
@@ -8,52 +8,72 @@ public class Day14Tests
     public Day14Tests(ITestOutputHelper output) => Output = output;
 
     [Fact] public void Step1WithExample() => new Day14Solver().ExecuteExample1("1588");
-        
+
     [Fact] public void Step2WithExample() => new Day14Solver().ExecuteExample2("2188189693529");
 
     [Fact] public void Step1WithPuzzleInput() => Output.WriteLine(new Day14Solver().ExecutePuzzle1());
-        
+
     [Fact] public void Step2WithPuzzleInput() => Output.WriteLine(new Day14Solver().ExecutePuzzle2());
 }
 
 public class Day14Solver : SolverBase
 {
     string Start;
-    Dictionary<(char Start, char End), char> Rules;
+    Dictionary<string, char> Rules;
 
     protected override void Parse(List<string> data)
     {
         var regex = new Regex("(?<First>.)(?<Second>.) -> (?<New>.)");
         Start = data[0];
 
-        Rules = data.Skip(2).Select(q => regex.Match(q)).ToDictionary(q => (q.Groups["First"].Value[0], q.Groups["Second"].Value[0]), q => q.Groups["New"].Value[0]);
+        Rules = data.Skip(2).Select(q => regex.Match(q)).ToDictionary(q => q.Groups["First"].Value + q.Groups["Second"].Value, q => q.Groups["New"].Value[0]);
     }
 
-    List<char> BuildChain(int count)
+    public static void AddCount<T>(Dictionary<T, long> countDictionary, T key, long value)
     {
-        var work = new LinkedList<char>(Start.ToCharArray());
-        for (int i = 0; i < count; i++)
+        countDictionary.TryGetValue(key, out var current);
+        countDictionary[key] = current + value;
+    }
+
+    // Solution was found after the hint to count pairs from reddit 
+
+    long BuildChain(int count)
+    {
+        var pairs = new Dictionary<string, long>();
+        for (int i = 0; i < Start.Length - 1; i++)
+            AddCount(pairs, Start.Substring(i, 2), 1);
+
+        var nextPairs = new Dictionary<string, long>();
+
+        for (int i = count; i > 0; i--)
         {
-            var item = work.First;
-            while (item.Next != null)
+            nextPairs.Clear();
+            foreach (var item in pairs)
             {
-                work.AddAfter(item, Rules[(item.Value, item.Next.Value)]);
-                item = item.Next.Next;
+                var ruleChar = Rules[item.Key];
+                var value1 = item.Key.Substring(0, 1) + ruleChar;
+                var value2 = ruleChar + item.Key.Substring(1, 1);
+                AddCount(nextPairs, value1, item.Value);
+                AddCount(nextPairs, value2, item.Value);
             }
+
+            var dummy = pairs;
+            pairs = nextPairs;
+            nextPairs = dummy;
         }
-        
-        return work.ToList();
+
+        var counter = new Dictionary<char, long>();
+        foreach (var item in pairs)
+            AddCount(counter, item.Key[1], item.Value);
+
+        AddCount(counter, Start.First(), 1);
+        return counter.Max(q => q.Value) - counter.Min(q => q.Value);
     }
 
     protected override object Solve1()
-    {
-        var data = BuildChain(10).GroupBy(q => q).Select(q => q.Count()).OrderBy(q => q).ToList();
-        return data.Last() - data.First();
-    }
+        => BuildChain(10);
 
     protected override object Solve2()
-    {
-        var data = BuildChain(40).GroupBy(q => q).Select(q => q.LongCount()).OrderBy(q => q).ToList();
-        return data.Last() - data.First();
-    }
+        => BuildChain(40);
+
 }
